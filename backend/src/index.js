dbb89b60d8488e1de68a556ea4a90dd7480871cc
Suman George <suman.g.mathew@gmail.com>
2026-09-24 11:34:42 +0530

    deployment updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
-
+import path from "path";
 dotenv.config();
 
 const port = process.env.PORT || 5001;
@@ -36,3 +36,11 @@ connectDB()
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
+}
